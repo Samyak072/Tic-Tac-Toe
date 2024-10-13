3e4eed943bdfc40d9a53f679beb5f6b775e0d3b5
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 13:15:19 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,37 @@
+// pages/_app.js
+
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import '../styles/globals.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Blue
+    },
+    secondary: {
+      main: '#dc004e', // Pink
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Tic Tac Toe</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+export default MyApp;
 
