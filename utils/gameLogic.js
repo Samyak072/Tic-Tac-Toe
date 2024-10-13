ddc70ce1665b353a16e5a068b418b0e4a2074033
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 13:13:34 +0530

    Update gameLogic.js

--- utils/gameLogic.js
+++ utils/gameLogic.js
@@ -1,1 +1,22 @@
+// utils/gameLogic.js
+
+export const calculateWinner = (board) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Rows
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columns
+    [0, 4, 8],
+    [2, 4, 6], // Diagonals
+  ];
+
+  for (let [a, b, c] of lines) {
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a];
+    }
+  }
+  return null;
+};
 
