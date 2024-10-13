db7891ab5f5b85231f39cb28d9fd44c765867f63
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 14:29:03 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,28 @@
+// components/Footer.js
+
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+const Footer = () => {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: '#1976d2', // Match the primary color
+        color: '#ffffff',
+        padding: '1rem 0',
+        position: 'fixed',
+        bottom: 0,
+        width: '100%',
+        textAlign: 'center',
+      }}
+    >
+      <Typography variant="body1">
+        Made by Samyak Mishra with <FavoriteIcon sx={{ color: 'red', verticalAlign: 'middle' }} />. Enjoy the game!
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
