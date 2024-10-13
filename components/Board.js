93e81d1de6e8f8ca272eadfbb0694399f37276bc
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 13:14:32 +0530

    Update Board.js

--- components/Board.js
+++ components/Board.js
@@ -1,1 +1,30 @@
+// components/Board.js
+
+import React from 'react';
+import { Grid, Paper } from '@mui/material';
+import Square from './Square';
+
+const Board = ({ board, onMove }) => {
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        padding: 2,
+        margin: 'auto',
+        maxWidth: 320,
+        backgroundColor: '#ffffff',
+      }}
+    >
+      <Grid container spacing={1}>
+        {board.map((value, index) => (
+          <Grid item xs={4} key={index}>
+            <Square value={value} onClick={() => onMove(index)} />
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
+  );
+};
+
+export default Board;
 
