792e69a418387644cfcd523ed4c52cbd4618f48b
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 14:11:51 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,37 @@
 // components/Header.js
 
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import { muteAll, unmuteAll } from '../utils/sound'; // Import mute functions
 
 const Header = () => {
+  const [isMuted, setIsMuted] = useState(false);
+
+  const handleMuteToggle = () => {
+    if (isMuted) {
+      unmuteAll();
+    } else {
+      muteAll();
+    }
+    setIsMuted(!isMuted);
+  };
+
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
         <SportsEsportsIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" component="div">
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Tic Tac Toe
         </Typography>
+        <IconButton color="inherit" onClick={handleMuteToggle}>
+          {isMuted ? <VolumeOffIcon /> : <VolumeUpIcon />}
+        </IconButton>
       </Toolbar>
     </AppBar>
   );
 };
 
 export default Header;
-
