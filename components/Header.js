7ee88060b44ba39101a4dc8038f198cdac9188cd
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 13:13:55 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,21 @@
+// components/Header.js
+
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+
+const Header = () => {
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <SportsEsportsIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" component="div">
+          Tic Tac Toe
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
 
