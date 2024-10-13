1b3db3d1dc3f76cb91c149c467b34ad827ccf460
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-13 13:12:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Add any additional configurations here
+};
+
+module.exports = nextConfig;
 
